6114b46c7c261ee7a1eb52388f35d8c3aa9c5c22
hphanvn <32984672+hphanvn@users.noreply.github.com>
2022-07-13 12:57:33 -0700

    works on desktop only

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 if (window.matchMedia("(max-width: 767px)").matches) {
-  $(window).on("scrollstart", function(){
-    /* Act on the event */
-    $("#projectCollapse").fadeIn('slow');
-  });
 } else {
   $(".showProjectLine").hover(function() {
     /* Stuff to do when the mouse enters the element */
