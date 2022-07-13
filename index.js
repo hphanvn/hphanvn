8ac247a154407226f820a59b8c97630dff851cbd
hphanvn <32984672+hphanvn@users.noreply.github.com>
2022-07-13 12:30:34 -0700

    add mobile mode

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-$(".showProjectLine").hover(function() {
-  /* Stuff to do when the mouse enters the element */
-  $("#projectCollapse").fadeIn('slow');
-}, function() {
-  /* Stuff to do when the mouse leaves the element */
-  $("#projectCollapse").mouseleave(function() {
-    $("#projectCollapse").fadeOut('slow');
+if (window.matchMedia("(max-width: 767px)").matches) {
+  $(window).scroll(function() {
+    /* Act on the event */
+    $("#projectCollapse").fadeIn('slow');
   });
-});
+} else {
+  $(".showProjectLine").hover(function() {
+    /* Stuff to do when the mouse enters the element */
+    $("#projectCollapse").fadeIn('slow');
+  }, function() {
+    /* Stuff to do when the mouse leaves the element */
+    $("#projectCollapse").mouseleave(function() {
+      $("#projectCollapse").fadeOut('slow');
+    });
+  });
+}
